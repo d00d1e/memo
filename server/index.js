dc6fd3f2b2874e7f91afa6b57e4b3f9ca68a0259
d00d1e <lee.julianay@gmail.com>
2021-10-18 16:19:38 -0700

    setup express server, connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// DATABASE
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
