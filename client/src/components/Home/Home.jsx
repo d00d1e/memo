ad7180fda3970a47d231d72a12487c41213d48ac
d00d1e <lee.julianay@gmail.com>
2021-10-25 19:23:34 -0700

    add search bar and chip input

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../redux/actions/postsActions";
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import ChipInput from "material-ui-chip-input";
 
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
@@ -17,11 +26,26 @@ const useQuery = () => {
 
 export default function Home() {
   const [currentId, setCurrentId] = useState(null);
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
   const user = JSON.parse(localStorage.getItem("profile"));
   const dispatch = useDispatch();
+  const history = useHistory();
   const query = useQuery();
   const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
   const classes = useStyles();
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      // search post here
+    }
+  };
+
+  const handleAddTag = (tag) => setTags([...tags, tag]);
+
+  const handleDeleteTag = (tagToDelete) =>
+    setTags(tags.filter((tag) => tag !== tagToDelete));
 
   useEffect(() => {
     dispatch(getPosts());
@@ -41,6 +65,29 @@ export default function Home() {
             <Posts user={user} setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                label="Search Memos"
+                variant="outlined"
+                value={search}
+                fullWidth
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyPress={handleKeyPress}
+              />
+              <ChipInput
+                style={{ margin: "10px 0" }}
+                label="Search tags"
+                variant="outlined"
+                value={tags}
+                onAdd={handleAddTag}
+                onDelete={handleDeleteTag}
+              />
+            </AppBar>
             <Form
               user={user}
               currentId={currentId}
